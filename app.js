6e70707ad77b8719a5f189385b847da3e28743c4
Sorakatop <alcocergonzalomatias@gmail.com>
2021-06-07 00:33:39 -0300

    Trabajo practico Formar, agradezco a los pibes que me ayudaron, gracias diego

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3040,()=>console.log('Servidor corriendo'));
+app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/index.html")));
+
+app.get("/babbage", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/babbage.html")));
+
+app.get("/berners-lee", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/berners-lee.html")));
+
+app.get("/clarke", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/clarke.html")));
+
+app.get("/hamilton", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/hamilton.html")));
+
+app.get("/hopper", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/hopper.html")));
+
+app.get("/lovelace", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/lovelace.html")));
+
+app.get("/turing", (req,res)=>res.sendFile(path.join(__dirname,"/DH-Heroes/views/turing.html")));
+
+
+
+app.use(express.static(path.join(__dirname, 'DH-Heroes')));
